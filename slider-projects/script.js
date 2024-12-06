7028b8fd3bee8b2d59cce225db0cee43324a3520
Pelin Hangişi <pelinhangisi@Pelins-MBP.home>
2024-12-07 00:51:10 +0300

    added fetch call and make images dynamic

--- slider-projects/script.js
+++ slider-projects/script.js
@@ -1,10 +1,43 @@
+const sliderEndpoint = 'https://picsum.photos/v2/list';
 const nextIcon = document.querySelector('.next');
 const prevIcon = document.querySelector('.prev');
 const imageContainer = document.querySelector('.imageContainer');
-const images = document.querySelectorAll('img');
 
 let currentImg = 1;
 let timeout;
+
+function fetchAndDisplayImages() {
+  fetch(sliderEndpoint)
+    .then((response) => response.json())
+    .then((data) => {
+      data.slice(25, 35).forEach((item) => {
+        const imgElement = document.createElement('img');
+        imgElement.src = item.download_url;
+        imgElement.alt = item.author;
+        imageContainer.appendChild(imgElement);
+      });
+
+      updateImg();
+    })
+    .catch((error) => {
+      console.error('Resimler yüklenirken bir hata oluştu:', error);
+    });
+}
+
+function updateImg() {
+  const images = document.querySelectorAll('.imageContainer img');
+
+  if (currentImg > images.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = images.length;
+  }
+  imageContainer.style.transform = `translateX(-${(currentImg - 1) * 900}px)`;
+  timeout = setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}
 
 prevIcon.addEventListener('click', () => {
   currentImg--;
@@ -18,17 +51,4 @@ nextIcon.addEventListener('click', () => {
   updateImg();
 });
 
-function updateImg() {
-  if (currentImg > images.length) {
-    currentImg = 1;
-  } else if (currentImg < 1) {
-    currentImg = images.length;
-  }
-  imageContainer.style.transform = `translateX(-${(currentImg - 1) * 900}px)`;
-  timeout = setTimeout(() => {
-    currentImg++;
-    updateImg();
-  }, 3000);
-}
-
-updateImg();
+fetchAndDisplayImages();
